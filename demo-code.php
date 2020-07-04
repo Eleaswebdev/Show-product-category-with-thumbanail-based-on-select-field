87ff496f48dec7ca1925b7be5204d8a9c31771be
Eleas Kanchon <44223562+Eleaswebdev@users.noreply.github.com>
2020-07-04 16:13:48 +0600

    Create demo-code.php
    Insert this code in your template

---
+++ demo-code.php
@@ -0,0 +1,19 @@
+  $taxonomyName = "product_cat";
+        $terms = get_terms($taxonomyName, array('parent' => 0, 'orderby' => 'slug', 'hide_empty' => false));
+		$terms = get_field('associated_category');
+		
+
+          foreach ($terms as $term) {
+
+            echo '<li><a href="' . get_term_link($term->name, $taxonomyName) . '">' . $term->name . '</a></li>';
+            $thumbnail_id = get_woocommerce_term_meta($term->term_id, 'thumbnail_id', true);
+	        // get the image URL for parent category
+	        $image = wp_get_attachment_url($thumbnail_id);
+	        // print the IMG HTML for parent category
+	        $image_url = wp_get_attachment_image_src($image,'thumbnail_id');   
+
+	        echo '<a href="'.get_term_link( $term ) .'"><img src="'.$image.'" alt="Status"></a><br>';
+
+	        echo '<a href="' . get_term_link($term->name, $taxonomyName) . '">View all ' . $term->name . '</a>';
+
+        }
